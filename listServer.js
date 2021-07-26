a744886a843f75cae0c6215ca108b8cde1a6b04a
Matt Coburn <coburn@dandere.net>
2021-07-26 17:06:50 +1000

    Backport logging fix from Gen 3 and bump revision

--- listServer.js
+++ listServer.js
@@ -31,21 +31,45 @@ const fs = require("fs");
 // Used to store our configuration file data.
 // ---------------
 var configuration;
+var logFile = "NodeListServer.log";
 
 // ---------------
 // Logging configuration. Feel free to modify.
 // ---------------
-log4js.configure({
-  appenders: {
-  	'console': { type: 'stdout' },
-    	default: { type: 'file', filename: 'NodeListServer.log', maxLogSize: 1048576, backups: 3, compress: true }
-  },
-  categories: {
-    default: { appenders: ['default', 'console'], level: 'debug' }
+
+// Bugfix: If Google App Engine is running NodeLS, we cannot write to the disk, since it's readonly.
+// So we run a check to ensure we can write, if not, we just log to console.
+
+// Check if we can write?
+fs.access(__dirname, fs.constants.W_OK, function(err) {
+  if(err){
+	console.warn("Can't write to disk; logging to file will be disabled!");
+	
+	// Configure Log4js
+	log4js.configure({
+	  appenders: {
+		'console': { type: 'stdout' },
+		default: { type: 'stdout' }
+	  },
+	  categories: {
+		default: { appenders: ['console'], level: 'debug' }
+	  }
+	});
+  } else {
+	log4js.configure({
+		appenders: {
+			'console': { type: 'stdout' },
+			default: { type: 'file', filename: logFile, maxLogSize: 1048576, backups: 3, compress: true }
+		},
+		categories: {
+			default: { appenders: ['default', 'console'], level: 'debug' }
+		}
+	});
   }
 });
 
-var loggerInstance = log4js.getLogger('NodeListServer');
+// Now we get the logger instance.
+var loggerInstance = log4js.getLogger('NodeLS');
 
 // Do we have a configuration file?
 if (fs.existsSync("config.ini")) {
@@ -380,7 +404,7 @@ expressApp.post("/add", apiAddToServerList);
 expressApp.post("/remove", apiRemoveFromServerList);
 
 // Finally, start the application
-console.log("Welcome to NodeListServer Generation 2 Revision 1");
+console.log("Welcome to NodeListServer Generation 2 Revision 2");
 console.log("Report bugs and fork the project on GitHub: https://github.com/SoftwareGuy/NodeListServer");
 
 expressApp.listen(configuration.Core.listenPort, () => console.log(`Server listening on port ${configuration.Core.listenPort}.`));
