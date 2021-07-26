78679c856dcc26a91119744c920c70bbc7423ff9
Matt Coburn <coburn@dandere.net>
2021-07-26 16:59:52 +1000

    Fix a exit that occurs when Log4js cannot write to disk
    Thanks to Yoan from the Mirror Discord for pointing this out!

--- NodeLS.js
+++ NodeLS.js
@@ -31,20 +31,43 @@ const fs = require("fs");
 // ---------------
 var configuration;
 var configFile = "./config.json";
-
+var logFile = "NodeListServer.log";
 // ---------------
 // Logging configuration. Feel free to modify.
 // ---------------
-log4js.configure({
-  appenders: {
-  	'console': { type: 'stdout' },
-    	default: { type: 'file', filename: 'NodeListServer.log', maxLogSize: 1048576, backups: 3, compress: true }
-  },
-  categories: {
-    default: { appenders: ['default', 'console'], level: 'debug' }
+
+// Bugfix: If Google App Engine is running NodeLS, we cannot write to the disk, since it's readonly.
+// So we run a check to ensure we can write, if not, we just log to console.
+
+// Check if we can write?
+fs.access(__dirname, fs.constants.W_OK, function(err) {
+  if(err){
+	console.warn("Can't write to disk; logging to file will be disabled!");
+	
+	// Configure Log4js
+	log4js.configure({
+	  appenders: {
+		'console': { type: 'stdout' },
+		default: { type: 'stdout' }
+	  },
+	  categories: {
+		default: { appenders: ['console'], level: 'debug' }
+	  }
+	});
+  } else {
+	log4js.configure({
+		appenders: {
+			'console': { type: 'stdout' },
+			default: { type: 'file', filename: logFile, maxLogSize: 1048576, backups: 3, compress: true }
+		},
+		categories: {
+			default: { appenders: ['default', 'console'], level: 'debug' }
+		}
+	});
   }
 });
 
+// Now we get the logger instance.
 var loggerInstance = log4js.getLogger('NodeListServer');
 
 // ---------------
