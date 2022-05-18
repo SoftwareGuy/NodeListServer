7a28141e4e5dda167ed69e38ec9a03b19bbbf546
yayza <y.aezah@gmail.com>
2022-05-18 13:48:37 -0700

    moved all validations to validators.js and created requestHandlers.js

--- listServer.js
+++ listServer.js
@@ -25,16 +25,16 @@ const REVISION_VER = 3;
 // ---------------
 // Require some essential libraries and modules.
 // ---------------
-const uuid = require("uuid");
-const loggerInstance = require("./lib/logger");
-const { configuration } = require("./lib/config");
-
-// Constant references to various modules.
 const expressServer = require("express");
 const expressApp = expressServer();
 
-// Security checks
+// - Import what we need from our other files
+const loggerInstance = require("./lib/logger"); // our logger instance
+const { generateUuid } = require("./lib/utils"); // some utils
+const { configuration } = require("./lib/config"); // our configuration
+const requestHandlers = require("./lib/requestHandlers"); // requestHandlers (they check all or any request we specify)
 
+// Security checks
 // - Rate Limiter
 // Note: We check if this is undefined as well as set to true, because we may be
 // using an old configuration ini file that doesn't have the new setting in it.
@@ -66,90 +66,20 @@ if (configuration.Auth.useAccessControl) {
 expressApp.use(expressServer.json());
 expressApp.use(expressServer.urlencoded({ extended: true }));
 
+// These are our gatekeepers, requests must pass through these before allowed to access the API.
+expressApp.use(requestHandlers.handleErrors);
+expressApp.use(requestHandlers.handleAllRequests);
+expressApp.use((req, res, next) =>
+  requestHandlers.handlePathSpecificRequests(req, res, next, knownServers, allowedServerAddresses)
+);
+
 // Server memory array cache.
 var knownServers = [];
 
-// Taken from https://melvingeorge.me/blog/check-if-string-is-valid-ip-address-javascript
-function checkIfValidIP(str) {
-  // Regular expression to check if string is a IP address
-  const regexExp =
-    /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi;
-
-  return regexExp.test(str);
-}
-
-// - Authentication
-// apiCheckKey: Checks to see if the client specified key matches.
-function apiCheckKey(clientKey) {
-  if (clientKey === configuration.Auth.communicationKey) return true;
-  else return false;
-}
-
-// apiIsKeyFromRequestIsBad: The name is a mouthful, but checks if the key is bad.
-function apiIsKeyFromRequestIsBad(req) {
-  if (typeof req.body.serverKey === "undefined" || !apiCheckKey(req.body.serverKey)) {
-    loggerInstance.warn(`${req.ip} used a wrong key: ${req.body.serverKey}`);
-    return true;
-  } else {
-    return false;
-  }
-}
-
-// - Sanity Checking
-// apiDoesServerExistByUuid: Checks if the server exists in our cache, by UUID.
-function apiDoesServerExistByUuid(uuid) {
-  var doesExist = knownServers.filter((server) => server.uuid === uuid.trim());
-
-  if (doesExist.length > 0) {
-    return true;
-  }
-
-  // Fall though.
-  return false;
-}
-
-// apiDoesServerExist: Checks if the server exists in our cache, by UUID.
-function apiDoesServerExistByName(name) {
-  var doesExist = knownServers.filter((server) => server.name === name.trim());
-
-  if (doesExist.length > 0) {
-    return true;
-  }
-
-  // Fall though.
-  return false;
-}
-
-// apiDoesThisServerExistByAddressPort: Checks if the server exists in our cache, by IP address and port.
-function apiDoesThisServerExistByAddressPort(ipAddress, port) {
-  var doesExist = knownServers.filter(
-    (servers) => servers.ip === ipAddress.trim() && servers.port === +port
-  );
-  if (doesExist.length > 0) {
-    return true;
-  }
-
-  // Fall though.
-  return false;
-}
-
-// -- Request Handling
-// denyRequest: Generic function that denies requests.
-function denyRequest(req, res) {
-  loggerInstance.warn(
-    `Request from ${req.ip} denied. Tried ${req.method} method on path: ${req.path}`
-  );
-  return res.sendStatus(400);
-}
-
 // apiGetServerList: This handler returns a JSON array of servers to the clients.
 function apiGetServerList(req, res) {
-  if (apiIsKeyFromRequestIsBad(req)) {
-    return res.sendStatus(400);
-  } else {
-    // Shows if keys match for those getting list server details.
-    loggerInstance.info(`${req.ip} accepted; communication key matched: '${req.body.serverKey}'`);
-  }
+  // Shows if keys match for those getting list server details.
+  loggerInstance.info(`${req.ip} accepted; communication key matched: '${req.body.serverKey}'`);
 
   // A client wants the server list. Compile it and send out via JSON.
   var serverList = [];
@@ -225,167 +155,47 @@ function apiUpdateServerInList(req, res, serverId) {
 
 // apiAddToServerList: Adds a server to the list.
 function apiAddToServerList(req, res) {
-  // Doorstopper.
-  if (apiIsKeyFromRequestIsBad(req)) return res.sendStatus(400);
-
-  var potentialExistingServer = false;
-  var potentialExistingServerId = "";
-
-  // Check with access control, if that's enabled.
-  if (configuration.Auth.useAccessControl && !allowedServerAddresses.includes(req.ip)) {
-    // It's not allowed. On ya bike mate.
-    loggerInstance.warn(`Request denied from ${req.ip}: Not in ACL.`);
-    return res.sendStatus(403); // 403 Forbidden
-  }
-
-  // Ensure we have post data body. If not, begone.
-  if (typeof req.body === "undefined") {
-    loggerInstance.warn(`Request denied from ${req.ip}: No POST body data?`);
-    return res.sendStatus(400);
-  }
-
   // Now do we have a UUID already?
   // If we do have a server UUID: Then it's possible we're trying to update the server entry.
   // If we do not have a server UUID: Then it's possible we're trying to register the server entry.
   // If neither, on your bike mate, ya ain't welcome here.
-  if (typeof req.body.serverUuid != "undefined") {
-    potentialExistingServer = true;
-    potentialExistingServerId = req.body.serverUuid.trim();
-  }
+  if (knownServers.some((server) => server.uuid === req.body.serverUuid?.trim()))
+    return apiUpdateServerInList(req, res, req.body.serverUuid.trim()); // Hand it over to the update routine.
 
-  // If this is a potential existing server...
-  if (potentialExistingServer) {
-    // Does it really exist, tho?
-    if (apiDoesServerExistByUuid(potentialExistingServerId)) {
-      // Hand it over to the update routine.
-      apiUpdateServerInList(req, res, potentialExistingServerId);
-    } else {
-      // Too bad. Go home.
-      loggerInstance.warn(
-        `Request from ${req.ip} denied: No such server with UUID '${potentialExistingServerId}'`
-      );
-      return res.sendStatus(400);
-    }
-  } else {
-    // Must be a new server trying to add itself.
-    // Check to make sure the server name isn't null.
-    if (typeof req.body.serverName === "undefined") {
-      loggerInstance.warn(`Request from ${req.ip} denied: Server name is null/undefined.`);
-      return res.sendStatus(400);
-    }
+  // Time to wrap things up.
+  var newServer = {
+    uuid: generateUuid(knownServers),
+    ip: req.ip,
+    name: req.body.serverName.trim(),
+    port: parseInt(req.body.serverPort, 10),
+  };
 
-    // If our security setting doesn't allow duplicate server names, then we should check to ensure that
-    // it doesn't clash.
-    if (
-      !configuration.Security.allowDuplicateServerNames &&
-      apiDoesServerExistByName(req.body.serverName)
-    ) {
-      loggerInstance.warn(
-        `Request from ${req.ip} denied: Server name clashes with an existing server name.`
-      );
-      return res.sendStatus(400);
-    }
+  // Extra field santitization
+  newServer["players"] = parseInt(req.body.serverPlayers, 10) || 0;
+  newServer["capacity"] = parseInt(req.body.serverCapacity, 10) || 0;
+  newServer["extras"] = req.body.serverExtras?.trim() || "";
+  newServer["lastUpdated"] =
+    Date.now() + configuration.Pruning.inactiveServerRemovalMinutes * 60 * 1000;
 
-    // Okay, the server name got past our basic checks, stash it.
-    // newServer.name = req.body.serverName.trim();
+  knownServers.push(newServer);
 
-    // Now we need to check to ensure the server port isn't out of bounds.
-    // Port 0 doesn't exist as per se, so we need to make sure we're valid.
-    if (
-      typeof req.body.serverPort === "undefined" ||
-      isNaN(req.body.serverPort) ||
-      req.body.serverPort < 1 ||
-      req.body.serverPort > 65535
-    ) {
-      loggerInstance.warn(
-        `Request from ${req.ip} denied: Server port is undefined, below 1 or above 65335.`
-      );
-      return res.sendStatus(400);
-    }
-
-    // Check for a sneaky IP address and port collison attempt.
-    // if(typeof req.body.serverIp != "undefined" && checkIfValidIP(req.body.serverIp)) .... todo
-    // queriedAddress = req.ip;
-
-    if (apiDoesThisServerExistByAddressPort(req.ip, req.body.serverPort)) {
-      loggerInstance.warn(
-        `Request from ${req.ip} denied: Server collision! We're attempting to add a server that's already known.`
-      );
-      return res.sendStatus(400);
-    }
-
-    // Time to wrap things up.
-    var newServer = {
-      uuid: generateUuid(),
-      ip: req.ip,
-      name: req.body.serverName.trim(),
-      port: parseInt(req.body.serverPort, 10),
-    };
-
-    // Extra field santitization
-    newServer["players"] =
-      typeof req.body.serverPlayers === "undefined" || isNaN(req.body.serverPlayers)
-        ? 0
-        : parseInt(req.body.serverPlayers, 10);
-    newServer["capacity"] =
-      typeof req.body.serverCapacity === "undefined" || isNaN(req.body.serverCapacity)
-        ? 0
-        : parseInt(req.body.serverCapacity, 10);
-    newServer["extras"] =
-      typeof req.body.serverExtras !== "undefined" ? req.body.serverExtras.trim() : "";
-    newServer["lastUpdated"] =
-      Date.now() + configuration.Pruning.inactiveServerRemovalMinutes * 60 * 1000;
-
-    knownServers.push(newServer);
-
-    // Log it and send back the UUID to the client - they'll need it for later.
-    loggerInstance.info(
-      `Handled add server request from ${req.ip}: ${newServer["uuid"]} ('${newServer["name"]}')`
-    );
-    return res.send(newServer["uuid"]);
-  }
+  // Log it and send back the UUID to the client - they'll need it for later.
+  loggerInstance.info(
+    `Handled add server request from ${req.ip}: ${newServer["uuid"]} ('${newServer["name"]}')`
+  );
+  return res.send(newServer["uuid"]);
 }
 
 // apiRemoveFromServerList: Removes a server from the list.
 function apiRemoveFromServerList(req, res) {
-  // Doorstopper.
-  if (apiIsKeyFromRequestIsBad(req)) return res.sendStatus(400);
-
-  // Are we using access control? If so, are they allowed to do this?
-  if (configuration.Auth.useAccessControl && !allowedServerAddresses.includes(req.ip)) {
-    // Not allowed.
-    loggerInstance.warn(
-      `Remove server request blocked from ${req.ip}. They are not known in our allowed IPs list.`
-    );
-    return res.sendStatus(403);
-  }
-
-  if (typeof req.body === "undefined") {
-    loggerInstance.warn(`Request from ${req.ip} denied: no POST data was provided.`);
-    return res.sendStatus(400);
-  }
-
-  // Server isn't specified?
-  if (typeof req.body.serverUuid === "undefined") {
-    loggerInstance.warn(`Request from ${req.ip} denied: Server UUID was not provided.`);
-    return res.sendStatus(400);
-  }
-
-  if (!apiDoesServerExistByUuid(req.body.serverUuid)) {
-    loggerInstance.warn(
-      `Request from ${req.ip} denied: Can't delete server with UUID '${req.body.serverUuid}' from cache.`
-    );
-    return res.sendStatus(400);
-  } else {
-    knownServers = knownServers.filter((server) => server.uuid !== req.body.serverUuid);
-    loggerInstance.info(
-      `Deleted server '${req.body.serverUuid}' from cache (requested by ${req.ip}).`
-    );
-    return res.send("OK\n");
-  }
+  knownServers = knownServers.filter((server) => server.uuid !== req.body.serverUuid);
+  loggerInstance.info(
+    `Deleted server '${req.body.serverUuid}' from cache (requested by ${req.ip}).`
+  );
+  return res.send("OK\n");
 }
 
-// Automatically remove servers when they haven't updated after the time specified in the config.ini
+// Automatically remove servers when they haven't updated after the time specified in lib/config.js
 async function removeOldServers() {
   knownServers = knownServers.filter((freshServer) => freshServer.lastUpdated >= Date.now());
   setTimeout(removeOldServers, configuration.Pruning.inactiveServerRemovalMinutes * 60 * 1000);
@@ -395,7 +205,6 @@ removeOldServers();
 
 // -- Start the application -- //
 // Attach the functions to each path we use with NodeLS.
-expressApp.get("/", denyRequest);
 expressApp.post("/list", apiGetServerList);
 expressApp.post("/add", apiAddToServerList);
 expressApp.post("/remove", apiRemoveFromServerList);
